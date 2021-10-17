54fdd118c62ff23baa2d559ebd081f9a23d74c18
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-16 23:09:22 -0400

    Final (nondescriptive) commit before submission

--- public/index.js
+++ public/index.js
@@ -1,79 +1,57 @@
-let budgetRecords = [];  // Holds all transactions
+let transactions = [];
 let myChart;
+let budgetVersion;
+let db;
 
-loadAllRecords();
+// Create a new db request for a "budget" database.
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
 
-// Loads all records from the indexedDB "budgetRecords"
-function loadAllRecords() {
-  var request = window.indexedDB.open("budgetRecords");
+request.onupgradeneeded = function (e) {
+  console.log('Upgrade needed in IndexDB');
 
-  request.onupgradeneeded = function (event) {
-    var db = event.target.result;
-    db.createObjectStore("budgetRecords", { keyPath: "id", autoIncrement: true });
-  };
+  const { oldVersion } = e;
+  const newVersion = e.newVersion || db.version;
 
-  request.onerror = function (event) {
-    console.log("Failure");
-  };
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
 
-  // If DB creation successful, create transaction to retrieve all information
-  request.onsuccess = function (event) {
-    var db = request.result;
-    var transaction = db.transaction(["budgetRecords"]);
-    var objectStore = transaction.objectStore("budgetRecords");
-    var retrieveReq = objectStore.getAll();
+  db = e.target.result;
 
-    // If retrieval succeeded, save all records in global variable
-    retrieveReq.onsuccess = function (event) {
-      budgetRecords = retrieveReq.result;
-      console.log(budgetRecords);
-    }
-  };
-}
-
-// Handles local creation of new budget record
-function sendTransaction(isAdding) {
-  let nameEl = document.querySelector("#t-name");
-  let amountEl = document.querySelector("#t-amount");
-  let errorEl = document.querySelector(".form .error");
-
-  // validate form
-  if (nameEl.value === "" || amountEl.value === "") {
-    errorEl.textContent = "Missing Information";
-    return;
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
   }
-  else {
-    errorEl.textContent = "";
+};
+
+request.onerror = function (e) {
+  console.log(`Woops! ${e.target.errorCode}`);
+};
+
+request.onsuccess = function (e) {
+  console.log('success');
+  db = e.target.result;
+
+  // Check if app is online before reading from db
+  if (navigator.onLine) {
+    console.log('Backend online! 🗄️');
+    checkDatabase();
   }
+};
 
-  // create record
-  let transaction = {
-    name: nameEl.value,
-    value: amountEl.value,
-    date: new Date().toISOString()
-  };
+fetch("/api/transaction")
+  .then(response => {
+    return response.json();
+  })
+  .then(data => {
+    // save db data on global variable
+    transactions = data;
 
-  // if subtracting funds, convert amount to negative number
-  if (!isAdding) {
-    transaction.value *= -1;
-  }
+    populateTotal();
+    populateTable();
+    populateChart();
+  });
 
-  // add to beginning of current array of data
-  budgetRecords.unshift(transaction);
-
-  // re-run logic to populate ui with new record
-  populateChart();
-  populateTable();
-  populateTotal();
-
-  // save to indexedDB
-  saveRecord(transaction);
-}
-
-// Handles recalculation and display of total
 function populateTotal() {
   // reduce transaction amounts to a single total value
-  let total = budgetRecords.reduce((total, t) => {
+  let total = transactions.reduce((total, t) => {
     return total + parseInt(t.value);
   }, 0);
 
@@ -81,12 +59,11 @@ function populateTotal() {
   totalEl.textContent = total;
 }
 
-// Handles population of record table
 function populateTable() {
   let tbody = document.querySelector("#tbody");
   tbody.innerHTML = "";
 
-  budgetRecords.forEach(transaction => {
+  transactions.forEach(transaction => {
     // create and populate a table row
     let tr = document.createElement("tr");
     tr.innerHTML = `
@@ -98,10 +75,9 @@ function populateTable() {
   });
 }
 
-// Handles drawing the record chart
 function populateChart() {
   // copy array and reverse it
-  let reversed = budgetRecords.slice().reverse();
+  let reversed = transactions.slice().reverse();
   let sum = 0;
 
   // create date labels for chart
@@ -137,35 +113,128 @@ function populateChart() {
   });
 }
 
-// Handles saving of new record to indexedDB "budgetRecords"
-function saveRecord(newRecord) {
-  var request = window.indexedDB.open("budgetRecords");
+function sendTransaction(isAdding) {
+  let nameEl = document.querySelector("#t-name");
+  let amountEl = document.querySelector("#t-amount");
+  let errorEl = document.querySelector(".form .error");
 
-  request.onupgradeneeded = function (event) {
-    var db = event.target.result;
-    db.createObjectStore('budgetRecords', {keyPath: 'id', autoIncrement:true});
+  // validate form
+  if (nameEl.value === "" || amountEl.value === "") {
+    errorEl.textContent = "Missing Information";
+    return;
+  }
+  else {
+    errorEl.textContent = "";
+  }
+
+  // create record
+  let transaction = {
+    name: nameEl.value,
+    value: amountEl.value,
+    date: new Date().toISOString()
   };
 
-  request.onerror = function (event) {
-    console.log("Failure");
-  };
+  // if subtracting funds, convert amount to negative number
+  if (!isAdding) {
+    transaction.value *= -1;
+  }
 
-  // If DB creation successful, create transaction to retrieve all information
-  request.onsuccess = function (event) {
-    var db = request.result;
-    var transaction = db.transaction(["budgetRecords"], "readwrite");
-    var objectStore = transaction.objectStore("budgetRecords");
-    console.log(objectStore);
-    var addRequest = objectStore.add(newRecord);
+  // add to beginning of current array of data
+  transactions.unshift(transaction);
 
-    // If retrieval succeeded, save all records in global variable
-    addRequest.onsuccess = function (event) {
-      console.log("success");
+  // re-run logic to populate ui with new record
+  populateChart();
+  populateTable();
+  populateTotal();
+
+  // also send to server
+  fetch("/api/transaction", {
+    method: "POST",
+    body: JSON.stringify(transaction),
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      if (data.errors) {
+        errorEl.textContent = "Missing Information";
+      }
+      else {
+        // clear form
+        nameEl.value = "";
+        amountEl.value = "";
+      }
+    })
+    .catch(err => {
+      // fetch failed, so save in indexed db
+      saveRecord(transaction);
+
+      // clear form
+      nameEl.value = "";
+      amountEl.value = "";
+    });
+}
+
+const saveRecord = (record) => {
+  console.log('Save record invoked');
+
+  // Create a transaction on the BudgetStore db with readwrite access
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+  // Access your BudgetStore object store
+  const store = transaction.objectStore('BudgetStore');
+
+  // Add record to your store with add method.
+  store.add(record);
+};
+
+function checkDatabase() {
+  console.log('check db invoked');
+
+  // Open a transaction on your BudgetStore db
+  let transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+  // access your BudgetStore object
+  const store = transaction.objectStore('BudgetStore');
+
+  // Get all records from store and set to a variable
+  const getAll = store.getAll();
+
+  // If the request was successful
+  getAll.onsuccess = function () {
+    // If there are items in the store, we need to bulk add them when we are back online
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((res) => {
+          // If our returned response is not empty
+          if (res.length !== 0) {
+            // Open another transaction to BudgetStore with the ability to read and write
+            transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+            // Assign the current store to a variable
+            const currentStore = transaction.objectStore('BudgetStore');
+
+            // Clear existing entries because our bulk add was successful
+            currentStore.clear();
+            console.log('Clearing store 🧹');
+          }
+        });
     }
   };
 }
 
-// Event Handlers for add and sub funds buttons
 document.querySelector("#add-btn").onclick = function () {
   sendTransaction(true);
 };
@@ -174,3 +243,5 @@ document.querySelector("#sub-btn").onclick = function () {
   sendTransaction(false);
 };
 
+// Listen for app coming back online
+window.addEventListener('online', checkDatabase);
